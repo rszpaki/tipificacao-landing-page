6061b0c31d2271014ce581a355cfeb4a9761f1f4
rszpaki <rafaelszpaki@gmail.com>
2026-08-14 21:15:02 -0300

    Landing page pronta para deploy

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+import { Navbar1 } from "@/components/navbar1";
+import { Hero115 } from "@/components/hero115";
+import { Logos18 } from "@/components/logos18";
+import { Feature43 } from "@/components/feature43";
+import { Testimonial9 } from "@/components/testimonial9";
+import { Pricing2 } from "@/components/pricing2";
+import { Cta10 } from "@/components/cta10";
+import { Footer2 } from "@/components/footer2";
+import { BookADemo1 } from "@/components/book-a-demo1";
+import { Feature1 } from "@/components/feature1";
+import { Feature17 } from "@/components/feature17";
+
+export default function LandingPage1() {
+  return (
+    <main className="flex min-h-screen w-full flex-col overflow-x-hidden">
+      <Navbar1 />
+      <Hero115 />
+      <Feature43 />
+      <Feature1 />
+      <Feature17 />
+      <BookADemo1 />
+      <Footer2 />
+    </main>
+  );
+}
